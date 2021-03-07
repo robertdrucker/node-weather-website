a68a8ba4d18d465d37b95056f91b6e87a0c36cc6
Robert Drucker <drucker01@bellsouth.net>
2021-03-06 23:44:29 -0500

    Added 'feels like' temperature to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.current.weather_descriptions[0] + ' and currently ' + body.current.temperature + ' degrees Fahrenheit.')
+            callback(undefined, body.current.weather_descriptions[0] + ' and currently ' + body.current.temperature + ' degrees Fahrenheit.  It feels like ' + body.current.feelslike + ' degrees.')
         }
     })
 }
